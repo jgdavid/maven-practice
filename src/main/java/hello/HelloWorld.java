68888c9bb9400c15313e4288060a218fd3e65abb
jgdavid <jdavid@qa.com>
2023-03-17 15:00:01 +0000

    added calc test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,5 +3,7 @@ package hello;
 public class HelloWorld {
 	public void main(String args[]) {
 		System.out.println("Hello, World");
+		// first try pulled it down so you couldn't import oit from git
+		// just had to import it from the filesystem instead
 	}
 }
